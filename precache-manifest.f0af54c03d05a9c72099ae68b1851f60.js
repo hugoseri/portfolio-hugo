18361061baaa8df7a993e4978ffc4c885cbb5961
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-06-30 12:48:24 +0000

    Deploy hugoseri/portfolio-hugo to github.com/hugoseri/portfolio-hugo.git:gh-pages

---
+++ precache-manifest.f0af54c03d05a9c72099ae68b1851f60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fdd0de80faeb06295dced10c6eb69c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e51a906a3c5f8ebe2ec7",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f656de9480a3317f2cbf",
+    "url": "/static/js/2.f603005b.chunk.js"
+  },
+  {
+    "revision": "e51a906a3c5f8ebe2ec7",
+    "url": "/static/js/main.d443754b.chunk.js"
+  },
+  {
+    "revision": "d82c7c7dcfc0cb55d651",
+    "url": "/static/js/runtime-main.0a10cedf.js"
+  }
+]);
